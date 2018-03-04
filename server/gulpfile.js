87a92e2c004ee219e7dcc38915f5848fb87c9aba
Sofartin <hrubymar10@gmail.com>
2018-03-04 20:11:32 +0100

    Fixes and utilizing

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyCSS = require('gulp-minify-css'),
     insert = require('gulp-insert'),
-    tap = require('gulp-tap');
+    tap = require('gulp-tap'),
+    rename = require("gulp-rename");;
 
 gulp.task('default', function() {
     gulp.src('./src/fb-default.css').pipe(minifyCSS()).pipe(tap(function (file,t) {
         var css = file.contents.toString('utf8');
-        gulp.src('./src/fb-default.js').pipe(uglify()).pipe(insert.prepend("var css='"+css+"';")).pipe(gulp.dest('./dist/'));
+        gulp.src('./src/fb.js').pipe(uglify()).pipe(insert.prepend("var css='"+css+"';")).pipe(rename(function(path){path.basename += "-default";})).pipe(gulp.dest('./dist/'));
     }));
     gulp.src('./src/fb-dark.css').pipe(minifyCSS()).pipe(tap(function (file,t) {
         var css = file.contents.toString('utf8');
-        gulp.src('./src/fb-dark.js').pipe(uglify()).pipe(insert.prepend("var css='"+css+"';")).pipe(gulp.dest('./dist/'));
+        gulp.src('./src/fb.js').pipe(uglify()).pipe(insert.prepend("var css='"+css+"';")).pipe(rename(function(path){path.basename += "-dark";})).pipe(gulp.dest('./dist/'));
     }));
 });
 
